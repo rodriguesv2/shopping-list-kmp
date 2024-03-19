ab71a7e59e2f66139b36d87581e05a1ea17d47e7
Rubens Rodrigues <rubens.rrodrigues@dimensa.com.br>
2024-03-19 18:42:28 -0300

    fixing sintax error

--- composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/MainActivity.kt
@@ -3,15 +3,6 @@ package com.jetbrains.kmpapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import com.jetbrains.kmpapp.presentation.atomic.molecules.ButtonMolecule
-import com.jetbrains.kmpapp.utils.lifecycle.LifecycleRegistry
-import com.jetbrains.kmpapp.utils.lifecycle.lifecycleCallBack
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- composeApp/src/iosMain/kotlin/com/jetbrains/kmpapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/jetbrains/kmpapp/MainViewController.kt
@@ -1,8 +1,5 @@
 package com.jetbrains.kmpapp
 
-import androidx.compose.ui.uikit.ComposeUIViewControllerDelegate
 import androidx.compose.ui.window.ComposeUIViewController
-import com.jetbrains.kmpapp.utils.lifecycle.LifecycleRegistry
-import com.jetbrains.kmpapp.utils.lifecycle.lifecycleCallBack
 
 fun MainViewController() = ComposeUIViewController { App() }
